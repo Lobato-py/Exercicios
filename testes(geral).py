a3ead355590150fa0f0ccd8e18819a5a88260aa1
Lobato-py <lobatoamorim1@gmail.com>
2026-01-11 19:30:26 -0300

    Update testes(geral).py

--- testes(geral).py
+++ testes(geral).py
@@ -1,10 +1,20 @@
 # exercício 1
 
-idade = 20
-if idade >= 18:
-    maior_de = 'sim'
-else:
-    maior_de = 'não'
+# idade = 20
+# if idade >= 18:
+#     maior_de = 'sim'
+# else:
+#     maior_de = 'não'
 
-print(idade)
-print(maior_de)
+# print(idade)
+# print(maior_de)
+
+# checagem de numero par ou impar da aula 8
+numero=input('Digite um numero: ')
+numero = int(numero)
+
+resultado = numero%2
+if resultado == 0:
+    print('par')
+else:
+    print('impar')
