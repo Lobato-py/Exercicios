d0a888377f964459ec2c68fe65fb4f5c7a1bf018
Lobato-py <lobatoamorim1@gmail.com>
2026-01-11 19:09:52 -0300

    exercício e "testes"

---
+++ exercício1(dnv).py
@@ -0,0 +1,37 @@
+'''
+Refazendo os exercícos antigos para treinar
+Minha resolução abaixo (tem umas firula a mais que quis botar pra treinar/ não foi pedido)
+'''
+
+# nome = 'Gustavo'
+# sobrenome = 'Lobato'
+# idade = 20
+# if idade >= 18:
+#     maior_de_idade = 'Sim'
+# else:
+#     maior_de_idade = 'Não'
+# ano_nascimento = 2025 - idade
+# altura_metros = 1.68
+
+# print('Nome:', nome)
+# print('Sobrenome:', sobrenome)
+# print('Idade:', idade)
+# print('Ano de nascimento:', ano_nascimento)
+# print('É maior de idade?', maior_de_idade )
+# print('Altura em metros:', altura_metros)
+
+# Resolução do professor:
+
+nome = 'Gustavo'
+sobrenome = 'Lobato'
+idade = 20
+ano_nascimento = 2025 - idade
+maior_de_idade = idade >= 18 # ele não usa if e else, porque ainda não foi visto no curso
+altura_metros = 1.68
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade )
+print('Altura em metros:', altura_metros)
---
+++ testes(geral).py
@@ -0,0 +1,10 @@
+# exercício 1
+
+idade = 20
+if idade >= 18:
+    maior_de = 'sim'
+else:
+    maior_de = 'não'
+
+print(idade)
+print(maior_de)
